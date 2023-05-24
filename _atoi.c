2efe1eeb3b630184e7d30e6cfeb73b3e10a5c3ea
JoJoGDS <george.latiko@gamil.com>
2023-05-24 23:26:41 +0200

    Update _atoi.c
    modifying

--- _atoi.c
+++ _atoi.c
@@ -1,28 +1,74 @@
-#include <stdio.h>
+#include "shell.h"
 
 /**
- * This function prints the Number sequence up to a given number.
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * @param n The number of numbers to print.
+ * Return: 1 if interactive mode, 0 otherwise
  */
-void print_number(int n) {
-  int a = 0, b = 1, c;
-
-  for (int i = 0; i < n; i++) {
-    c = a + b;
-    printf("%d\n", c);
-    a = b;
-    b = c;
-  }
+int interactive(info_t *info)
+{
+  return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-int main() {
-  int n;
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+  while (*delim)
+    if (*delim++ == c)
+      return (1);
+  return (0);
+}
 
-  printf("Enter the number of numbers to print: ");
-  scanf("%d", &n);
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
 
-  print_number(n);
+int _isalpha(int c)
+{
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (1);
+  else
+    return (0);
+}
 
-  return 0;
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+  int i, sign = 1, flag = 0, output;
+  unsigned int result = 0;
+
+  for (i = 0; s[i] != '\0' && flag != 2; i++)
+  {
+    if (s[i] == '-')
+      sign *= -1;
+
+    if (s[i] >= '0' && s[i] <= '9')
+    {
+      flag = 1;
+      result *= 10;
+      result += (s[i] - '0');
+    }
+    else if (flag == 1)
+      flag = 2;
+  }
+
+  if (sign == -1)
+    output = -result;
+  else
+    output = result;
+
+  return (output);
 }
